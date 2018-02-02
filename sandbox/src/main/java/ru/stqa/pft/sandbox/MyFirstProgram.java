82d8aea32f28cf5f0c4c89e22c166f157102453a
KorJane <lastena@ukr.net>
2018-02-02 16:54:55 +0200

    new class MyFirstProgram created

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,10 +1,14 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram{
+/**
+ * Created by a.a.kornilov on 2/2/2018.
+ */
+public class MyFirstProgram {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-
-		System.out.println("Hello, world!");
-	}
-	
+        System.out.println("Hello, world!");
+        System.out.println(1.0 / 2);
+    }
 }
+
+
