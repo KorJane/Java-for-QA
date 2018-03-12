6c385bfe2285cad5ccf4d02a2c3e86ff36516728
KorJane <lastena@ukr.net>
2018-03-12 15:30:58 +0200

    add json

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
@@ -10,16 +11,22 @@ import java.util.stream.Collectors;
 public class ContactData {
     @XStreamOmitField
     private  int id;
+    @Expose
     private String firstName;
+    @Expose
     private String lastName;
     private String nickName;
+    @Expose
     private String address;
+    @Expose
     private String email;
     private String email2;
     private String email3;
+    @Expose
     private String homePhone;
     private String mobilePhone;
     private String workPhone;
+    @Expose
     private String group;
     private String allPhones;
     private String allEmails;
--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -24,7 +26,7 @@ import static org.testng.Assert.assertEquals;
 public class ContactCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validContacts() throws IOException {
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(new File ("src/test/resources/contacts.xml")));
         String xml = "";
         String line = reader.readLine();
@@ -34,14 +36,26 @@ public class ContactCreationTests extends TestBase {
         }
         XStream xStream = new XStream();
         xStream.processAnnotations(ContactData.class);
-
-//        List<ContactData> contacts = (List<ContactData>)xStream.fromXML(xml);
         List<ContactData> contacts = (List<ContactData>)xStream.fromXML(xml);
-
         return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
 
-    @Test(dataProvider = "validContacts")
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File ("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null){
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+
+
+    @Test(dataProvider = "validContactsFromJson")
      public void testContactCreation(ContactData contact){
         app.goTO().homePage();
         Contacts before = app.contact().all();
